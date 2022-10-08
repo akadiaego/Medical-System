dc88dc63f60f939ffdcccdffe751622197799da5
akadiaego <printfhxr@gmail.com>
2022-10-08 22:03:56 +0800

    'v0'

---
+++ src/main/java/com/hospital/ray/yygh_parent/YyghParentApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.ray.yygh_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/ray/yygh_parent/YyghParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.ray.yygh_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
